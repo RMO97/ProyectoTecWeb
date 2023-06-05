7da62ef72a26d35fd2aa36a842c8c17171ed33bb
RMO97 <belmont2097@gmail.com>
2023-06-05 14:08:12 -0600

    Se agregó bootstrap
    Se agrego bootstrap al proyecto

--- client-api/resources/views/clientes.blade.php
+++ client-api/resources/views/clientes.blade.php
@@ -1,29 +1,35 @@
-<h1> Clientes </h1>
-<table>
-    <thead>
-        <tr>
-            <th>Nombre</th>
-            <th>Telefono</th>
-            <th>Direccion</th>
-            <th>Email</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($data as $cliente)
+@extends('layout/plantilla')
+@section('tituloPagina', 'Clientes index')
+@section('contenido')
+<div>
+    <h1> Clientes </h1>
+    <table>
+        <thead>
             <tr>
-                <td>{{ $cliente['nombre'] }}</td>
-                <td>{{ $cliente['telefono'] }}</td>
-                <td>{{ $cliente['direccion'] }}</td>
-                <td>{{ $cliente['email'] }}</td>
-                
-                <td>
-                    <a href="{{ route('cliente.view', $cliente['id']) }}">Ver</a>
-                    <a href="{{ route('cliente.delete', $cliente['id']) }}">Borrar</a>
-                </td>
+                <th>Nombre</th>
+                <th>Telefono</th>
+                <th>Direccion</th>
+                <th>Email</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
-<a href="{{ route('productos.index') }}">Productos</a>
-<a href="{{ route('ventas.index') }}">Ventas</a>
-<a href="{{ route('clientes.store') }}">Crear nuevo cliente</a>
+        </thead>
+        <tbody>
+            @foreach ($data as $cliente)
+                <tr>
+                    <td>{{ $cliente['nombre'] }}</td>
+                    <td>{{ $cliente['telefono'] }}</td>
+                    <td>{{ $cliente['direccion'] }}</td>
+                    <td>{{ $cliente['email'] }}</td>
+                    
+                    <td>
+                        <a href="{{ route('cliente.view', $cliente['id']) }}">Ver</a>
+                        <a href="{{ route('cliente.delete', $cliente['id']) }}">Borrar</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <a href="{{ route('productos.index') }}">Productos</a>
+    <a href="{{ route('ventas.index') }}">Ventas</a>
+    <a href="{{ route('clientes.store') }}">Crear nuevo cliente</a>
+</div>
+@endsection
